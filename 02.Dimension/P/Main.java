dd23a588b101f2a5048a3836d23d881761fcbbae
Arpit <75125324+arpittaneja@users.noreply.github.com>
2021-06-03 12:12:03 +0530

    Add files via upload

---
+++ 02.Dimension/P/Main.java
@@ -0,0 +1,12 @@
+package P;
+import P1.*;
+import P2.*;
+
+public class Main {
+    public static void main(String[] args) {
+        TwoDim obj0 = new TwoDim(1, 2);
+        System.out.println(obj0);
+        ThreeDim obj1 = new ThreeDim(3, 4, 5);
+        System.out.println(obj1);
+    }
+}
