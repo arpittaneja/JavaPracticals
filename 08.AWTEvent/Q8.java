3868095ff6eeab767b31ed7a6e11cea22edd5e10
Arpit <75125324+arpittaneja@users.noreply.github.com>
2021-07-17 14:21:15 +0530

    Update and rename mouse.java to Q8.java

---
+++ 08.AWTEvent/Q8.java
@@ -0,0 +1,36 @@
+/*8. Write	 a	 program	 to	 create	 a	 frame using	 AWT.	 Implement	 mouseClicked(),	
+mouseEntered()	and	mouseExited()	events such	that:
+a) Size	of	the	frame	should	be	tripled	when	mouse	enters it.
+b) Frame	should	reduce	to	its	original	size	when	mouse	is	clicked	in	it.
+c) Close	the	frame	when	mouse	exits it.*/
+
+import java.awt.*; 
+import java.awt.event.*;
+public class Q8 implements MouseListener{
+	Frame f;
+	Q8(){
+    	f=new Frame("Q6");
+f.setSize(300,300); 
+f.setLayout(null); 
+f.setVisible(true); 
+f.addMouseListener(this);
+	} 
+	public void mouseClicked(MouseEvent e) { 
+f.setSize(300,300);
+	} 
+	public void mouseEntered(MouseEvent e) { 
+f.setSize(900,900);
+	} 
+	public void mouseExited(MouseEvent e) { 
+f.dispose();
+	} 
+	public void mousePressed(MouseEvent e) { 
+ 
+	} 
+	public void mouseReleased(MouseEvent e) { 
+ 
+	} 
+public static void main(String[] args) { 
+	new Q8(); 
+} 
+}
