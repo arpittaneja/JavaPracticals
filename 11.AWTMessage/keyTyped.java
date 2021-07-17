78ee2164a53543f8c605868d19a795f9fe9c6889
Arpit <75125324+arpittaneja@users.noreply.github.com>
2021-07-17 15:23:39 +0530

    Update keyTyped.java

--- 11.AWTMessage/keyTyped.java
+++ 11.AWTMessage/keyTyped.java
@@ -5,40 +5,40 @@ frame window when the user types any key.
 
 
 
-import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 public class keyTyped extends KeyAdapter
 {
-   JFrame f;
-   JLabel label;
-   keyTyped()
-   {
-       f = new JFrame();
-       label = new JLabel();
-       label.setBounds(25, 35, 200, 100);
-       label.setBackground(Color.LIGHT_GRAY);
-       label = new JLabel("", SwingConstants.CENTER);
-       f.add(label);
-       f.addKeyListener(this);
-       f.setFocusable(true);
-       f.setSize(300,300);
-       f.setVisible(true);
-
-
-   }
-
-   // 3 overrided methods of class KeyAdapter
-   public void keyPressed(KeyEvent e) { }
-   public void keyReleased(KeyEvent e) { }
-   public void keyTyped(KeyEvent e)
-   {
-       char ch = e.getKeyChar();
-       label.setText("Typed character is: "+ch);
-   }
-
-   public static void main(String snrao[])
-   {
-       new keyTyped();
-   }
+    Frame f;
+    Label label;
+    keyTyped()
+    {
+        f = new Frame();
+        label = new Label();
+        label.setBounds(25, 35, 200, 100);
+        label.setBackground(Color.LIGHT_GRAY);
+        label.setAlignment(label.CENTER);
+        f.add(label);
+        f.addKeyListener(this);
+        f.setFocusable(true);
+        f.setSize(300,300);
+        f.setVisible(true);f.addWindowListener(new WindowAdapter() {
+        public void windowClosing (WindowEvent e) 
+        {
+        f.dispose();
+        }
+        });
+    }
+    // 3 overrided methods of class KeyAdapter
+    public void keyPressed(KeyEvent e) { }
+    public void keyReleased(KeyEvent e) { }
+    public void keyTyped(KeyEvent e)
+    {
+        char ch = e.getKeyChar();
+        label.setText("Typed character is: "+ch);
+    }
+    public static void main(String snrao[])
+    {
+        ew keyTyped();
+    }
 }
